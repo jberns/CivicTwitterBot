8e3f6b8b9cff03795d15731e84f7d46f72f0062f
Aurora Ziobrowski <aziobrow@gmail.com>
2018-08-11 16:02:12 -0600

    Added database and questions model

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/civic_twitter_bot_development'
+db = SQLAlchemy(app)
+
+# Create our database model
+class Question(db.Model):
+    __tablename__ = "questions"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(140), unique=True)
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return '<Content %r>' % self.content
 
 @app.route('/')
 def hello():
